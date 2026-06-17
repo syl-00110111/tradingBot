d35cfbda01c63e871726ce599f7371c4e6858820
Sylvain <131753108+syl-00110111@users.noreply.github.com>
2026-06-17 22:53:11 +0200

    Swaps less & go faster

--- bot.py
+++ bot.py
@@ -384,7 +384,7 @@ class AnalysisWorker(threading.Thread):
             try:
                 cpu_usage = psutil.cpu_percent(interval=0.1)
                 mem_available = psutil.virtual_memory().available
-                if cpu_usage < 40 and mem_available > footprint*2:
+                if cpu_usage < 40 and mem_available > footprint*max_workers:
                     max_workers += 1
             except: pass
 
